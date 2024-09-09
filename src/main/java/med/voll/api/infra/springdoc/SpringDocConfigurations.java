7998cab9d2884d061f79e6a18452867e81b73d27
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-09-09 11:22:31 -0300

    Authorization in SpringDoc documentation (Swagger)

---
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,27 @@
+package med.voll.api.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Voll.med.API")
+                        .description("API Rest para clínicas médicas")
+                        .contact(new Contact()
+                                .name("Leo Fabiano Alves")
+                                .email("leo.fabiano@gmail.com")));
+    }
+}
